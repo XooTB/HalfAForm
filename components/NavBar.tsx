de663e349ba34c411080b351baa4d5e5f0a13008
Samiul Alim <xo.ot2001@gmail.com>
2024-10-02 22:57:23 +0600

    Initial Commit With HomePage

---
+++ components/NavBar.tsx
@@ -0,0 +1,39 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import ThemeToggle from "./ThemeToggle";
+
+type Props = {};
+
+const NavBar = ({}: Props) => {
+  return (
+    <div className="flex justify-between items-center px-32 py-5">
+      <h1 className="text-2xl capitalize font-semibold">Formly</h1>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Home</NavigationMenuTrigger>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Docs</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Introduction</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div>
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
