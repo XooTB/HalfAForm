570f96bc01b71e4a26d3d823231dd6a8b8fe2316
Samiul Alim <xo.ot2001@gmail.com>
2024-10-10 23:07:02 +0600

    Feat: Added Authentication for full Dashboard routes

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,20 +1,34 @@
+"use client";
+
 import BreadCrumbs from "@/components/sections/BreadCrumbs";
 import DashboardMenu from "@/components/sections/DashboardMenu";
 import type { Metadata } from "next";
+import { useSession } from "next-auth/react";
 import { Inter } from "next/font/google";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Dashboard",
-};
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/login");
+    },
+  });
+
+  if (!session) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center text-xl font-bold">
+        Redirecting...
+      </div>
+    );
+  }
+
   return (
     <div className="w-full flex">
       <DashboardMenu />
