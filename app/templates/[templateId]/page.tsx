752f033fb2aa585af3cb034a6a8515511c856d13
Samiul Alim <xo.ot2001@gmail.com>
2024-10-12 19:30:20 +0600

    Feat: Added /templates and /templates/id routes

---
+++ app/templates/[templateId]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center text-2xl font-bold">
+      Template Preview
+    </div>
+  );
+};
+
+export default page;
---
+++ app/templates/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Templates</h1>
+    </div>
+  );
+};
+
+export default page;
