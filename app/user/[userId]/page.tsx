18d3f31d23a517a20ba72c0691e5dca2a1010128
Samiul Alim <xo.ot2001@gmail.com>
2024-11-01 22:08:21 +0600

    Fix: Updated User Profile Page

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -3,22 +3,36 @@ import Image from "next/image";
 import { getServerSession } from "next-auth/next";
 import authOptions from "@/lib/providers";
 
+export const metadata = {
+  title: "User Profile",
+  description: "User Profile",
+};
+
 // Change this to an async function
 async function page({ params }: { params: { userId: string } }) {
   const { userId } = params;
+  // Dynamic placeholder for the user's avatar
   const robohashUrl = `https://robohash.org/${userId}?set=set3`;
 
   // Get the session server-side
   const session = await getServerSession(authOptions);
 
+  const user = await fetch(`${process.env.NEXT_PUBLIC_API}/users/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
+
+  const userData = await user.json();
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="max-w-4xl mx-auto bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+      <div className="max-w-4xl mx-auto bg-muted shadow-lg rounded-lg overflow-hidden py-5">
         <div className="md:flex px-5">
           <div className="md:flex-shrink-0">
             <Image
               className="h-48 w-full object-cover md:w-48 rounded-full border-2 border-gray-200 dark:border-gray-700"
-              src={robohashUrl}
+              src={userData.avatar || robohashUrl}
               alt="Profile"
               width={192}
               height={192}
@@ -26,11 +40,9 @@ async function page({ params }: { params: { userId: string } }) {
           </div>
           <div className="p-8">
             <p className="uppercase tracking-wide text-indigo-500 dark:text-indigo-400 font-semibold text-2xl">
-              {session?.user?.name || "Anonymous User"}
+              {userData.name || "Anonymous User"}
             </p>
-            <h1 className="mt-2 text-xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-              User Profile
-            </h1>
+            <p className="text-muted-foreground text-sm">{userData.email}</p>
           </div>
         </div>
 
@@ -51,16 +63,23 @@ async function page({ params }: { params: { userId: string } }) {
             My Templates
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {[1, 2, 3].map((template) => (
+            {userData.authored.map((template: any) => (
               <div
-                key={template}
+                key={template.id}
                 className="bg-white dark:bg-gray-600 p-4 rounded-lg shadow"
               >
-                <h3 className="font-semibold text-lg mb-2 dark:text-white">
-                  Template {template}
+                <Image
+                  src={template.image}
+                  alt={template.name}
+                  width={100}
+                  height={100}
+                  className="w-full h-48 object-cover rounded-lg"
+                />
+                <h3 className="font-semibold text-lg mb-2 dark:text-white line-clamp-1 pt-3">
+                  {template.name}
                 </h3>
-                <p className="text-gray-600 dark:text-gray-300">
-                  A brief description of this amazing template.
+                <p className="text-gray-600 dark:text-gray-300 text-sm line-clamp-3">
+                  {template.description}
                 </p>
               </div>
             ))}
--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -5,19 +5,17 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const UserProfile = () => {
   const { data: session } = useSession();
-  const router = useRouter();
 
   return (
     <>
@@ -26,7 +24,10 @@ const UserProfile = () => {
           <DropdownMenuTrigger>
             <Avatar className="border-2 border-gray-200">
               <AvatarImage
-                src={`https://robohash.org/${session.user?.id}?set=set3`}
+                src={
+                  session.user?.image ||
+                  `https://robohash.org/${session.user?.id}?set=set3`
+                }
                 alt={session.user?.name || "User avatar"}
               />
               <AvatarFallback>CN</AvatarFallback>
@@ -55,14 +56,15 @@ const UserProfile = () => {
           </DropdownMenuContent>
         </DropdownMenu>
       ) : (
-        <Button
-          onClick={() => router.push("/login")}
-          className={`
+        <Link href="/login">
+          <Button
+            className={`
             bg-white border text-black hover:bg-slate-100
             dark:bg-gray-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800`}
-        >
-          Login
-        </Button>
+          >
+            Login
+          </Button>
+        </Link>
       )}
     </>
   );
