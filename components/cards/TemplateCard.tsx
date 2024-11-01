243f2608a929ce7361bf1134d3b8ff17fc65719e
Samiul Alim <xo.ot2001@gmail.com>
2024-11-01 22:24:13 +0600

    Fix: Updated Template Card

--- components/cards/TemplateCard.tsx
+++ components/cards/TemplateCard.tsx
@@ -11,6 +11,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
 
 interface TemplateCard extends Template {
   id: string;
@@ -22,32 +23,35 @@ type Props = {
 
 const TemplateCard = ({ template }: Props) => {
   return (
-    <Card className="hover:cursor-pointer hover:shadow-lg transition-all duration-300 bg-card dark:bg-dark-card">
-      <CardContent>
+    <Card className="hover:cursor-pointer hover:shadow-lg transition-all duration-300">
+      <CardHeader className="p-0 rounded-t-md relative">
         <Image
-          src="/placeholders/template_img.png"
+          src={template.image}
           alt="template image"
           width={100}
           height={100}
-          className="w-full h-full object-cover"
-          priority={true}
+          className="w-full h-48 object-cover rounded-t-md"
           placeholder="empty"
+          loading="lazy"
         />
-      </CardContent>
-      <CardHeader>
+        <Badge className="absolute top-1 right-2" variant="secondary">
+          {template.status}
+        </Badge>
+      </CardHeader>
+      <CardContent className="px-2 py-4 justify-start">
         <CardTitle>{template.name}</CardTitle>
-        <CardDescription className="text-sm text-muted-foreground line-clamp-2">
+        <CardDescription className="text-sm text-muted-foreground line-clamp-2 pt-2 pb-4">
           {template.description}
         </CardDescription>
-      </CardHeader>
-      <CardFooter className="flex gap-2">
-        <Link href={`/templates/${template.id}`} target="_blank">
-          <Button variant="outline">View</Button>
-        </Link>
-        <Link href={`/dashboard/templates/edit/${template.id}`}>
-          <Button>Edit</Button>
-        </Link>
-      </CardFooter>
+        <CardFooter className="gap-4 justify-start w-full p-0">
+          <Link href={`/templates/${template.id}`} target="_blank">
+            <Button variant="outline">View</Button>
+          </Link>
+          <Link href={`/dashboard/templates/edit/${template.id}`}>
+            <Button>Edit</Button>
+          </Link>
+        </CardFooter>
+      </CardContent>
     </Card>
   );
 };
